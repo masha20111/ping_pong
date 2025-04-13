1f7747171525a791ae4b84ae0dd422d197a61db9
masha20111 <trifonovamasa442@gmail.com>
2025-04-13 13:27:54 +0500

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -1,38 +1,40 @@
 from pygame import *
-from random import randint
-from time import time as timer
+# from random import randint
+# from time import time as timer
 window = display.set_mode((700, 500))
-display.set_caption("Пинг Понг")
-background = window.fill((255,255,255))
+display.set_caption("Пинг понг")
+window.fill((255,255,255))
+# background = transform.scale(image.load("galaxy.jpg"), (700, 500))
 
-
+speed_x = 3
+speed_y = 3
 # mixer.init()
 # mixer.music.load('space.ogg')
 # mixer.music.play()
 # fire_sound = mixer.Sound('fire.ogg')
 # #wingame = mixer.Sound('')
 # #kick = mixer.Sound('')
-# class GameSprite(sprite.Sprite):
-#     def __init__(self, player_image, player_x, player_y, player_speed,size_x, size_y):
-#         super().__init__()
-#         self.image = transform.scale(image.load(player_image), (size_x, size_y))
-#         self.speed = player_speed
-#         self.rect = self.image.get_rect()
-#         self.rect.x = player_x
-#         self.rect.y = player_y
-#     def reset(self):
-#         window.blit(self.image, (self.rect.x, self.rect.y))
+class GameSprite(sprite.Sprite):
+     def __init__(self, player_image, player_x, player_y, player_speed,size_x, size_y):
+          super().__init__()
+          self.image = transform.scale(image.load(player_image), (size_x, size_y))
+          self.speed = player_speed
+          self.rect = self.image.get_rect()
+          self.rect.x = player_x
+          self.rect.y = player_y
+     def reset(self):
+          window.blit(self.image, (self.rect.x, self.rect.y))
 
-# class Player (GameSprite):
-#     def update(self):
-#         keys_pressed = key.get_pressed()
-#         if keys_pressed[K_LEFT] and self.rect.x > 5:
-#             self.rect.x -= self.speed
-#         if keys_pressed[K_RIGHT] and self.rect.x < 635 :
-#             self.rect.x += self.speed
-#     def fire(self):
-#         bullet = Bullet("bullet.png", self.rect.centerx, self.rect.top, 15, 20, 15) 
-#         bullets.add(bullet) 
+class Player (GameSprite):
+     def update(self):
+          keys_pressed = key.get_pressed()
+          if keys_pressed[K_UP] and self.rect.y > 5:
+               self.rect.y -= self.speed
+          if keys_pressed[K_DOWN] and self.rect.y < 635 :
+               self.rect.y += self.speed
+     # def fire(self):
+     #      bullet = Bullet("bullet.png", self.rect.centerx, self.rect.top, 15, 20, 15) 
+     #      bullets.add(bullet) 
 # lost = 0
 # class Enemy(GameSprite):
 #     direction = "left"
@@ -55,6 +57,7 @@ background = window.fill((255,255,255))
 #         self.rect.y -= self.speed
 #         if self.rect.y < 0:
 #             self.kill()
+
             
 # bullets = sprite.Group()       
 # monsters = sprite.Group()
@@ -75,10 +78,11 @@ background = window.fill((255,255,255))
 # sprites_list = sprite.groupcollide( 
 # monsters, bullets, True, True 
 # )
-
+win_height = 500
 clock = time.Clock()
 FPS = 60
-# player = Player('rocket.png', 5, 370, 10, 100, 130)
+player = Player('platform.png', 5, 370, 10, 130, 130)
+ball = GameSprite('ball.png', 5, 370, 10, 100, 100)
 # score = 0
     
 finish = False
@@ -90,9 +94,9 @@ game = True
 # rel_time = False 
 while game:
   
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
+     for e in event.get():
+          if e.type == QUIT:
+               game = False
 #         elif e.type == KEYDOWN:
 #             if e.key == K_SPACE:
 #                 fire_sound.play()
@@ -107,23 +111,31 @@ while game:
 #                     if num_fire >= 5 and rel_time == False: 
 #                         last_time = timer() 
 #                         rel_time = True 
-#     if finish != True:
+     if finish != True:
+          window.fill((255,255,255))
+          ball.rect.x += speed_x
+          ball.rect.y += speed_y
+          if ball.rect.y > win_height-50 or ball.rect.y < 0:
+               speed_y *= -1
 #         text = font1.render("Счёт: " + str(score), 1, (255,255,255))
 #         text_lose = font1.render("Пропущено:" + str(lost), 1,(255,255,255))
         
 #         window.blit(background,(0, 0))
 #         window.blit(text_lose,(10,50))
-#         window.blit(text,(10,20))
+          #window.blit(ball)
 #         monsters.draw(window)   
 #         bullets.draw(window) 
 #         asteroids.draw(window)  
+          #ball.draw(window)  
 
 #         bullets.update()     
 #         monsters.update()
 #         asteroids.update()
-#         player.update()
+          player.update()
+          ball.update()
  
-#         player.reset()
+          ball.reset()
+          player.reset()
 
 #         if rel_time == True: 
 #             now_time = timer() 
@@ -181,6 +193,6 @@ while game:
 #             asteroid = Asteroid('asteroid.png',randint(50,450),-40,randint(1,5),100,100)
 #             asteroids.add(asteroid)
         
-    display.update()
-    
-    clock.tick(FPS) 
+     display.update()
+
+     clock.tick(FPS)
